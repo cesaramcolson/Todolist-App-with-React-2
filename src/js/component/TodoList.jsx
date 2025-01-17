a324462b6f2d0d3be3b57a00e1c6a0d78a532331
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-01-17 19:49:51 +0000

    Styles and general structure

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TodoList = () => {
+	return (
+		<>
+			<h1>todos</h1>
+			<div className="list">
+				<ul>
+					<li>My first todo</li>
+					<li>My first todo</li>
+					<li>My first todo</li>
+				</ul>
+			</div>
+			<div className="borderOne"></div>
+			<div className="borderTwo"></div>
+		</>
+	);
+};
+
+export default TodoList;
