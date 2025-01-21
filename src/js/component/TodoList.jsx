a6cb93390dc4b10fb519dac5f6919ffa4e6f9a1c
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-01-21 05:07:47 +0000

    got the list going, still need to be able to delete tasks and a task counter

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -7,21 +7,25 @@ const TodoList = () => {
 		<>
 			<h1>todos</h1>
 			<div className="list">
-				<input 
+				<ul>
+					<li><input 
 					type="text" 
 					className="form-control" 
 					placeholder="What needs to be done?"
+					value={newTask}
 					onChange={(e) => setNewTask(e.target.value)}
 					onKeyDown={(e) => {
 						if (e.key === 'Enter') {
 							setTaskList([...taskList, newTask])
+							setNewTask('')
 						}
 					}}
-				/>
-				<ul>
-					<li>My first todo</li>
-					<li>My first todo</li>
-					<li>My first todo</li>
+				/></li>
+					{taskList.map((index, item) => (
+						<li>
+							<span>{index}</span>
+						</li>
+					))}
 				</ul>
 			</div>
 			<div className="borderOne"></div>
