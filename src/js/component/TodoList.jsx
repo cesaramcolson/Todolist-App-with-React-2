265751713bb9e9112339eced32f6a3fb83536ffa
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-01-21 04:45:59 +0000

    setting the useSate functions

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 const TodoList = () => {
+	const [ taskList , setTaskList ] = useState([])
+	const [ newTask , setNewTask ] = useState("")
 	return (
 		<>
 			<h1>todos</h1>
 			<div className="list">
-				<input type="text" className="form-control" placeholder="What needs to be done?"/>
+				<input 
+					type="text" 
+					className="form-control" 
+					placeholder="What needs to be done?"
+					onChange={(e) => setNewTask(e.target.value)}
+					onKeyDown={(e) => {
+						if (e.key === 'Enter') {
+							setTaskList([...taskList, newTask])
+						}
+					}}
+				/>
 				<ul>
 					<li>My first todo</li>
 					<li>My first todo</li>
